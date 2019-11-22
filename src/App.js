609451cb291c4da2a876c982dd5b0d44207a7c34
Timm Schoenborn <timmschoenborn@gmail.com>
2019-11-22 17:59:08 -0600

    Login and Register Component works

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import LoginRegisterForm from './LoginRegisterForm'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          REACT INITIALIZED! Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	constructor(){
+		super()
+
+		this.state = {
+    		loggedin: false,
+    		loggedInUsername: null
+    	}
+	}
+	login = async (loginInfo) => {
+		console.log("\nThis is loginInfo", loginInfo);
+		const response = await fetch(process.env.REACT_APP_API_URL + '/api/v1/users/login', {
+			method: 'POST',
+			credentials: 'include',
+			body: JSON.stringify(loginInfo),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+		const parsedLoginResponse = await response.json()
+		console.log(parsedLoginResponse);
+		if(parsedLoginResponse.status.code === 200) {
+			this.setState({
+				loggedin: true,
+				loggedInUsername: parsedLoginResponse.data.username
+			})
+		} else {
+			console.log("Login Failed");
+			console.log(parsedLoginResponse);
+		}
+	}
+	register = async (loginInfo) => {
+		console.log("\nThis is loginInfo");
+		const response = await fetch(process.env.REACT_APP_API_URL + '/api/v1/users/register', {
+			method: 'POST',
+			credentials: 'include',
+			body: JSON.stringify(loginInfo),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+		const parsedLoginResponse = await response.json()
+		console.log(parsedLoginResponse);
+		if(parsedLoginResponse.status.code === 201) {
+			this.setState({
+				loggedin: true,
+				loggedInUsername: parsedLoginResponse.data.username
+			})
+		} else {
+			console.log("Register Failed");
+			console.log(parsedLoginResponse);
+		}
+	}
+	render(){
+		return(
+			<div className="App">
+				<LoginRegisterForm login={this.login} register={this.register} />
+			</div>
+		)
+	}
 }
 
 export default App;
---
+++ src/LoginRegisterForm/index.js
@@ -0,0 +1,113 @@
+import React from 'react'
+import { Form, Button, Label } from 'semantic-ui-react';
+
+class LoginRegisterForm extends React.Component{
+	constructor(){
+		super()
+
+		this.state = {
+			email: '',
+			username: '',
+			password: '',
+			action: 'login'
+		}
+	}
+	handleChange = (e) => {
+		this.setState({[e.currentTarget.name]: e.currentTarget.value})
+	}
+	loginRegister = () => {
+		if(this.state.action === 'login'){
+			this.props.login({
+				username: this.state.username,
+				password: this.state.password
+			})
+		} else {
+			this.props.register({
+				email: this.state.email,
+				username: this.state.username,
+				password: this.state.password
+			})
+		}
+	}
+	switchForm = () => {
+		if (this.state.action === 'login') {
+			this.setState({
+				action: 'register'
+			})
+		} else {
+			this.setState({
+				action: 'login'
+			})
+		}
+	}
+	handleSubmit = (e) => {
+		e.preventDefault()
+		this.loginRegister()
+	}
+	render(){
+		return(
+			<div className="LoginRegisterForm">
+				<Form onSubmit={this.handleSubmit}>
+					{
+						this.state.action === 'register'
+						?
+						<React.Fragment>
+							<Label>Email:</Label>
+							<Form.Input
+								type="email"
+								name="email"
+								value={this.state.email}
+								onChange={this.handleChange}
+							/>
+						</React.Fragment>
+						:
+						null
+					}
+					<Label>Username:</Label>
+					<Form.Input
+						type="text"
+						name="username"
+						value={this.state.username}
+						onChange={this.handleChange}
+					/>
+					<Label>Password:</Label>
+					<Form.Input
+						type="password"
+						name="password"
+						value={this.state.password}
+						onChange={this.handleChange}
+					/>
+					<Button type="Submit">{this.state.action ==="register" ? "Register" : "Log In"}</Button>
+				</Form>
+				{
+					this.state.action === "register"
+					?
+					<small>Already have an account? Log in <span onClick={this.switchForm}>here</span>!</small>
+					:
+					<small>Need an account? Register <span onClick={this.switchForm}>here</span>!</small>
+				}
+			</div>
+		)
+	}
+}
+
+
+
+
+
+
+export default LoginRegisterForm
+
+
+
+
+
+
+
+
+
+
+
+
+
+
